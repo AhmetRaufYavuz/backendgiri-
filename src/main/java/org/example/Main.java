494181077e160af90958ed514aee1dbe15e1c7e5
Ahmet Rauf Yavuz <ahmetraufyavuz@hotmail.com>
2024-09-10 19:09:05 +0300

    ödev tamamlandı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,6 @@
 package org.example;
+import static java.lang.Math.PI;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -6,22 +8,35 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23 || !isBarking) return false;
+        return clock < 8 || clock >= 20;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       if( firstAge <=19 && firstAge >=13)return true;
+       if( secondAge <=19 && secondAge >=13)return true;
+       if( thirdAge <=19 && thirdAge >=13)return true;
+       return false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        int limit = isSummer ? 45:35;
+        return  temp >= 25 && temp <= limit;
     }
 
     public static double area(double width, double height) {
-       
+       if (width < 0 || height <0 ){
+           System.out.println("width or height cannot be less than zero!");
+           return -1;
+       }
+       return width*height;
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0 ){
+            System.out.println("radius cannot be less than zero!");
+            return -1;
+        }
+        return radius*radius*Math.PI;
     }
 }
